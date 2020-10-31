4b11d3c7d45a7db0b82d5ada78c16e05d0f72454
Student <student@Techs-MacBook-Air.local>
2020-10-31 11:16:44 -0400

    push with finished get cat image method

--- src/main/java/com/techelevator/services/RestCatPicService.java
+++ src/main/java/com/techelevator/services/RestCatPicService.java
@@ -1,16 +1,25 @@
 package com.techelevator.services;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestClientResponseException;
+import org.springframework.web.client.RestTemplate;
 
 import com.techelevator.model.CatPic;
+
 
 @Component
 public class RestCatPicService implements CatPicService {
 
+	RestTemplate restTemplate = new RestTemplate();
+	
 	@Override
 	public CatPic getPic() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		CatPic catPics = null;
+             catPics = restTemplate.getForObject("https://random-cat-image.herokuapp.com/", CatPic.class);
+             return catPics;
+
 	}
 
 }	
